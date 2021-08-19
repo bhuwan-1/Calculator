2af11fd675f74be9b7d13ea7c90713fd7cf38c32
bhuwan-1 <ayeitsbhuws@gmail.com>
2021-08-19 22:26:06 +0600

    Added comment to check commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Adding this comment to experiment with commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
